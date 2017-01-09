6232f8cb1e45efb5f2e82d0413bedcb45e9fd4e1
Chyld Medford <chyld.medford@gmail.com>
2017-01-09 12:59:31 +0530

    wip:

--- src/Sum.js
+++ src/Sum.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 export default (props) => {
-  let sum = props.a + props.b;
+  let sum = props.nums.reduce((acc, cur) => acc + cur);
   return (
     <div>
       <h1>Sum</h1>
+      <ul>
+      {
+        props.nums.map((num, index) => <li key={index}>{num}</li>)
+      }
+      </ul>
       <p>
-        {props.a} + {props.b} = {sum}
+        {sum}
       </p>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Basic from './Basic';
 import Sum from './Sum';
+import Counter from './Counter';
 import './index.css';
 
 ReactDOM.render(
   <div>
-    <Sum a={3} b={2} />
-    <Sum a={4} b={4} />
-    <Sum a={5} b={6} />
-    <Sum a={6} b={8} />
+    <Counter />
+    <Counter />
   </div>,
   document.getElementById('root')
 );
